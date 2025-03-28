7607e230c28cb922553dd52b0353df484cd31852
User <alex22zm1@gmail.com>
2025-03-28 21:31:15 +0300

    1

--- controls.py
+++ controls.py
@@ -1,6 +1,6 @@
 import pygame, sys
-
-def events(gun):
+from bullet import  Bullet
+def events(screen, gun, bullets):
     """Обработка событий"""
     for event in pygame.event.get():  # цикл который постоянно мониторит
         # события пользователя
@@ -12,14 +12,27 @@ def events(gun):
                 gun.mright = True
             elif event.key == pygame.K_a: #влево
                 gun.mleft = True
+            elif event.key == pygame.K_SPACE:
+                new_bullet = Bullet(screen, gun)
+                bullets.add(new_bullet)
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_d:
                 gun.mright = False
             elif event.key == pygame.K_a:
                 gun.mleft = False
 
-def update(bg_color, screen, gun):
+def update(bg_color, screen, gun, bullets):
     """Обновление экрана"""
     screen.fill(bg_color)  # заполняет экран указанным цветом
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
     gun.output()
     pygame.display.flip()
+
+def update_bullets(bullets):
+    """обновлять позиции пуль"""
+    bullets.update()
+    for bullet in bullets.copy():
+        if bullet.rect.bottom <= 0:
+            bullets.remove(bullet)
+    print(len(bullets))
