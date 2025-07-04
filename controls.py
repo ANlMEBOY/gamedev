ab7c00719cb3008558a54ccc4decfb417e9cc815
User <alex22zm1@gmail.com>
2025-07-05 01:55:37 +0300

    1

--- controls.py
+++ controls.py
@@ -1,5 +1,7 @@
 import pygame, sys
 from bullet import  Bullet
+from ino import Ino
+
 def events(screen, gun, bullets):
     """Обработка событий"""
     for event in pygame.event.get():  # цикл который постоянно мониторит
@@ -21,12 +23,13 @@ def events(screen, gun, bullets):
             elif event.key == pygame.K_a:
                 gun.mleft = False
 
-def update(bg_color, screen, gun, bullets):
+def update(bg_color, screen, gun, inos, bullets):
     """Обновление экрана"""
     screen.fill(bg_color)  # заполняет экран указанным цветом
     for bullet in bullets.sprites():
         bullet.draw_bullet()
     gun.output()
+    inos.draw(screen)
     pygame.display.flip()
 
 def update_bullets(bullets):
@@ -35,4 +38,15 @@ def update_bullets(bullets):
     for bullet in bullets.copy():
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
-    print(len(bullets))
+
+def create_army(screen, inos):
+    """создание армии пришельцев"""
+    ino = Ino(screen)
+    ino_width = ino.rect.width
+    number_ino_x = int((1200 - 2 * ino_width) / ino_width)
+
+    for ino_number in range(number_ino_x):
+        ino = Ino(screen)
+        ino.x = ino_width + ino_width * ino_number
+        ino.rect.x = ino.x
+        inos.add(ino)
--- lol_kek.py
+++ lol_kek.py
@@ -4,17 +4,19 @@ from pygame.sprite import Group
 
 def run():
     pygame.init() #инициализируем модуль пайгейм
-    screen = pygame.display.set_mode((700, 500)) #обозначили разрешение экрана
+    screen = pygame.display.set_mode((1200, 600)) #обозначили разрешение экрана
     pygame.display.set_caption("Космические защитники") #название окна игры
     bg_color = (0, 0, 0) #цвет экрана
     gun = Gun(screen)#добавили пушку
     bullets = Group()
+    inos = Group()
+    controls.create_army(screen, inos)
 
     while True:
         controls.events(screen, gun, bullets)
         gun.update_gun()
         bullets.update()
-        controls.update(bg_color, screen, gun, bullets)
+        controls.update(bg_color, screen, gun, inos, bullets)
         controls.update_bullets(bullets)
 
 run()
